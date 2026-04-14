f6558c72e537c9ed9d97a252908e395275d5561c
psimichelalves-ai <psi.michelalves@gmail.com>
2026-04-14 02:12:28 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "metacog-app";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(["./"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});

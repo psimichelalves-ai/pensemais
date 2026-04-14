6c8a81e58c919c40021d1f3aa343ab76fdef6d7e
psimichelalves-ai <psi.michelalves@gmail.com>
2026-04-14 03:44:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "metacog-app";
+const CACHE_NAME = "pensemais";
 
 self.addEventListener("install", e => {
   e.waitUntil(
@@ -10,8 +10,6 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
